df0d1c5fdbc414b6ceec77c44745567bd85e4609
qiye <jianghaoyang36@163.com>
2019-09-21 22:06:43 +0800

    请求封装优化

--- web_back/js/utils/myuser.js
+++ web_back/js/utils/myuser.js
@@ -3,19 +3,19 @@
   //专门定义一个对象，该对象包含各种用户所需方法
     var obj ={
         login:function(userName,password,callback){
-            $.post('http://localhost:8000/admin/login',{user_name:userName,password:password},function(res){
+            $.post(qingqiu.user_login,{user_name:userName,password:password},function(res){
                   callback(res);
              
             })
         },
         getInfo:function(callback){
-             $.get('http://localhost:8000/admin/getuser',function(res){
+             $.get(qingqiu.user_getInfo,function(res){
                    console.log(res);
                     callback(res);
              })
         },
            logout:function(callback){
-              $.post('http://localhost:8000/admin/logout',function(res){
+              $.post(qingqiu.user_logout,function(res){
                     callback(res)
               })
            }
